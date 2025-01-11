19d9c0c206feb99b89b7bcfd15296f6c3fee12e5
ArihantJainWebDev <arihantjainwebdev@gmail.com>
2025-01-11 09:25:10 +0530

    final touch

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
 import './Main.css'
 import { assets } from '../../assets/assets'
 import { Context } from '../../context/Context'
 
 const Main = () => {
-
     const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context)
 
     return (
@@ -14,7 +14,6 @@ const Main = () => {
                 <img src={assets.user_icon} alt="user icon" />
             </div>
             <div className="main-container">
-
                 {!showResult ?
                     <>
                         <div className='greet'>
@@ -22,21 +21,21 @@ const Main = () => {
                             <p>How can I help you today?</p>
                         </div>
                         <div className="cards">
-                            <div className="card">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, fuga?</p>
+                            <div className="card" key="guide">
+                                <p>Explore the universe of Gemini with our comprehensive guides.</p>
                                 <img src={assets.compass_icon} alt="compass icon" />
                             </div>
-                            <div className="card">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, fuga?</p>
-                                <img src={assets.bulb_icon} alt="compass icon" />
+                            <div className="card" key="tips">
+                                <p>Get insights and tips on using Gemini effectively.</p>
+                                <img src={assets.bulb_icon} alt="bulb icon" />
                             </div>
-                            <div className="card">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, fuga?</p>
-                                <img src={assets.message_icon} alt="compass icon" />
+                            <div className="card" key="support">
+                                <p>Reach out to our support team for any queries.</p>
+                                <img src={assets.message_icon} alt="message icon" />
                             </div>
-                            <div className="card">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, fuga?</p>
-                                <img src={assets.code_icon} alt="compass icon" />
+                            <div className="card" key="updates">
+                                <p>Stay updated with the latest features and updates.</p>
+                                <img src={assets.code_icon} alt="code icon" />
                             </div>
                         </div>
                     </>
@@ -60,7 +59,7 @@ const Main = () => {
                 }
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a promt here...' />
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here...' />
                         <div>
                             <img src={assets.gallery_icon} alt="gallery icon" />
                             <img src={assets.mic_icon} alt="mic icon" />
@@ -68,12 +67,22 @@ const Main = () => {
                         </div>
                     </div>
                     <p className='bottom-info'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. A soluta commodi harum aliquid provident eaque. A, perspiciatis. Officiis, fugiat? Amet quia ex non ipsum hic quisquam nemo praesentium quibusdam omnis.
+                        Discover the full potential of Gemini. We offer a wide range of resources to help you get the most out of our platform. Enjoy a seamless experience and excellent support.
                     </p>
                 </div>
             </div>
         </div>
     )
+}
+
+Main.propTypes = {
+    onSent: PropTypes.func,
+    recentPrompt: PropTypes.string,
+    showResult: PropTypes.bool,
+    loading: PropTypes.bool,
+    resultData: PropTypes.string,
+    setInput: PropTypes.func,
+    input: PropTypes.string
 }
 
 export default Main
